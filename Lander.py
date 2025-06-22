c2863f1cb5a74b58eff23c6b575b0b7b91da81d8
Soumyosundar <83171604+Soumyosundar@users.noreply.github.com>
2025-06-22 10:28:37 -0400

    Lander.py

--- Lander.py
+++ Lander.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-# --- Same DQN Architecture ---
+# DQN Architecture
 class DQN(nn.Module):
     def __init__(self, state_dim, action_dim):
         super(DQN, self).__init__()
@@ -18,7 +18,7 @@ class DQN(nn.Module):
     def forward(self, x):
         return self.net(x)
 
-# --- Load environment and model ---
+# Loading Environment and Model
 env = gym.make("LunarLander-v3", render_mode="human")
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.n
@@ -27,7 +27,7 @@ q_net = DQN(state_dim, action_dim)
 q_net.load_state_dict(torch.load("lunarlander_dqn.pth"))
 q_net.eval()
 
-# --- Evaluation Loop ---
+# Evalution loop
 state, _ = env.reset()
 done = False
 total_reward = 0
